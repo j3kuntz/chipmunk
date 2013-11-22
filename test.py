32af7476e3d64caa85516dad0b914189de23e38a
Joshua Kuntz <j3kuntz@gmail.com>
2013-11-21 22:55:46 -0800

    Actually archive files

--- test.py
+++ test.py
@@ -1,25 +1,21 @@
-import boto
-import time
-import argparse
-import os
-import sqlite3
-import bz2
-
 from contextlib import closing
 from zipfile import ZipFile, ZIP_DEFLATED
-import os
-
-    
-import shutil
-
+import argparse
+import boto
+import bz2
+import datetime
 import logging
+import os
+import os
+import shutil
+import sqlite3
+import time
 
 logger = logging.getLogger("chipmunk")
 
 ACCESS_KEY_ID       = "AKIAIUTI6QAVHWRD6CDA"
 SECRET_ACCESS_KEY   = "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"
 VALUT               = "TEST_VALUT_1"
-TEMP_WORK_DIR       = ".chipmunk_temp"
 
 '''
 # boto.connect_glacier is a shortcut return a Layer2 instance 
@@ -38,73 +34,73 @@ print archive_id
 '''
 
 def walk_files_in_directory(directory_path):
-    assert os.path.exits(directory_path), "Path doesn't Exist: %s " % directory_path
+    assert os.path.exists(directory_path), \
+        "Path doesn't Exist: %s " % directory_path
     for dirname, dirnames, filenames in os.walk(directory_path):
         for filename in filenames:
             yield os.path.join(dirname, filename)
 
-def zipdir(basedir, archivename):
-    assert os.path.isdir(basedir)
-    with closing(ZipFile(archivename, "w", ZIP_DEFLATED)) as z:
-        for root, dirs, files in os.walk(basedir):
-            for fn in files:
-                absfn = os.path.join(root, fn)
-                zfn = absfn[len(basedir)+len(os.sep):]
-                z.write(absfn, zfn)
+def put_file_in_glacier(filehandle, vault):
+    archive_id = vault.upload_archive(filehandle)
+    return archive_id
 
-def bzip_file(filehandle):
-    outfile = filehandle + ".bz2"
-    with open(infile, 'rb') as input:
-        with bz2.BZ2File(outfile, 'wb', compresslevel=9) as output:
-            shutil.copyfileobj(input, output)
-    return outfile
+def archive_glacier(filehandle, vault, db_conn, directory, tag):
+    archive_id  = put_file_in_glacier(filehandle, vault)
+    _, filename = os.path.split(filehandle)
+    db_conn.execute("""
+        INSERT INTO archived_files VALUES (?, ?, ?, ?, ?, ?);
+    """, [
+        filename,
+        filehandle,
+        tag,
+        archive_id,
+        vault.name,
+        datetime.datetime.utcnow(),
+    ])
+    db_conn.commit()
 
-def compress_directory_for_archive(directory_path):
-    '''
-        Takes a directory and returns a path to a 
-        compressed version of it
-    '''
-    logging.debug("Compressing Directory: %s", directory_path)
-    assert os.path.exists(directory_path), "Directory doesn't exist: %s" % directory_path
-    assert os.path.isdir(directory_path), "Path is not a directory: %s" % directory_path
-    _, dirname = os.path.split(directory_path)
-    logging.debug("Dirname: %s", dirname)
-    final_result_path = os.path.join(TEMP_WORK_DIR, dirname + ".zip")
-    logging.debug("Destination path: %s", dest_path)
-    zipdir(directory_path, final_result_path)
-    return final_result_path
-
-def archive_directory(directory_to_archive):
-    logger.debug("Archiving Directory: %s", directory_to_archive)
-    compressed_path = compress_directory_for_archive(directory_to_archive)
-    logging.debug("Compressed Directory: %s", compressed_path)
+def archive_directory(db_conn, vault, directory, tag):
+    logger.debug("Archiving Directory: %s", directory)
+    for file_handle in walk_files_in_directory(directory):
+        archive_glacier(file_handle, vault, db_conn, directory, tag)
 
 def configure():
     parser = argparse.ArgumentParser(description='Upload a directory to glacier')
     parser.add_argument('--directory', type=str, help='directory to upload')
-
+    parser.add_argument('--tag', type=str, help='tag the files')
+    parser.add_argument('--db', type=str, help='location of sqlite3 db')
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    return parser.parse_args()
 
-    shutil.rmtree(TEMP_WORK_DIR)
-    os.mkdir(TEMP_WORK_DIR)
-    
-    glacier_connection = boto.connect_glacier(
+def connect(db):
+    glacier_conn = boto.connect_glacier(
       aws_access_key_id     = ACCESS_KEY_ID,
       aws_secret_access_key = SECRET_ACCESS_KEY,
     )
-    return parser.parse_args()
 
-def connect_to_glacier(aws_access_key, awe_secret):
-    return 
+    db_conn = sqlite3.connect(db)
+    return db_conn, glacier_conn
+
+def prep_db(db_conn):
+    # Create table if it doesn't exist
+    db_conn.execute('''
+        CREATE TABLE IF NOT EXISTS archived_files
+        (name text, path text, tag text, archive_id text, vault text, archived_dt datetime);
+    ''')
 
 def main():
     args = configure()
 
-    directory_to_archive = args.directory
+    directory = args.directory
+    tag       = args.tag
+    sqlitedb  = args.db
 
-    archive_directory(directory_to_archive)
+    db_conn, glacier_conn = connect(sqlitedb)
+    prep_db(db_conn)
 
-    # shutil.rmtree(TEMP_WORK_DIR)
+    vault = glacier_conn.create_vault(VALUT)
+
+    archive_directory(db_conn, vault, directory, tag)
 
 if __name__ == "__main__":
     main()
